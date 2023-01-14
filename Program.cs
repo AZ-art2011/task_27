3fb893980b4609dcf74c02a6698b832601fce7dd
Alexander <mypost@example.com>
2023-01-14 18:16:03 +0300

    2

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
+int val1 = num;
+int val2 = 0;
+
+for (int i = 10; num/i != 0; i *= 10)
+{
+    val2 = val2 + val1 % 10;
+    val1 = val1/10;
+}
+
+val2 = val2 + val1;
+Console.WriteLine(val2);
+
+
